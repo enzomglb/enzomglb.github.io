b8d2f94b738686bbe742343d4e7711a8f27e66b8
enzomglb <enzo.maserati+nant@globant.com>
2022-03-15 23:29:35 -0300

    add push-worker

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1647396067.966083
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://cdn.shopify.com/s/files/1/0249/9326/7772/products/logo_grande_grande_a5b034b5\u002D8b86\u002D47bc\u002Daf3c\u002Deba114fdea8b_600x.jpg?v\u003D1574291320',
+  defaultTitle: 'Default Title Test',
+  defaultActionURL: 'https://latimes.com',
+  appKey: 'ZNG41J3pQ6G9kXHrPdUAaQ',
+  token: 'MTpaTkc0MUozcFE2RzlrWEhyUGRVQWFROkFKX19kQXJGeDhqNFBZb1ltbTIydEJXazJYNXY4VUt4U1JtSjhoYVdzdnc',
+  vapidPublicKey: 'BGDJ_2k06TotXoTTG5FXNOnlqxvg3uIO5TGDgXl-yPkSD_YElRT5qO_MuZFUJ2Gnez56f-umkXsn4CiekbljGdg='
+})
